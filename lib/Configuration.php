39393fb0de45e37eb594b0c11740d95e492529bb
pumpkinball <91833653+pumpkinball@users.noreply.github.com>
2023-07-04 15:11:05 +0100

    Update Configuration.php

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -157,7 +157,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = '[xero-php-oauth2 (2.22.0)]';
+    protected $userAgent = '[xero-php-oauth2 (2.23.0)]';
 
     /**
      * Debug switch (default set to false)
@@ -697,7 +697,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    OpenAPI Spec Version: 2.38.0' . PHP_EOL;
-        $report .= '    SDK Package Version: 2.22.0' . PHP_EOL;
+        $report .= '    SDK Package Version: 2.23.0' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
