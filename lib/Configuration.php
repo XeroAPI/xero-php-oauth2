a49d568c58fd304ad00f31e8ebd691b51896e171
sangeet-joy_xero <sangeet.joy@xero.com>
2024-08-26 12:35:18 +0530

    Build from OAS 6.2.0

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -157,7 +157,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = '[xero-php-oauth2 (7.1.1)]';
+    protected $userAgent = '[xero-php-oauth2 (7.2.0)]';
 
     /**
      * Debug switch (default set to false)
@@ -696,8 +696,8 @@ class Configuration
         $report  = 'PHP SDK (XeroAPI\XeroPHP) Debug Report:' . PHP_EOL;
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
-        $report .= '    OpenAPI Spec Version: 6.1.1' . PHP_EOL;
-        $report .= '    SDK Package Version: 7.1.1' . PHP_EOL;
+        $report .= '    OpenAPI Spec Version: 6.2.0' . PHP_EOL;
+        $report .= '    SDK Package Version: 7.2.0' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
