cac7c77b2cf9b140efbf146541f207aa2df8eb0e
sangeet-joy_xero <sangeet.joy@xero.com>
2025-04-23 11:24:28 +0530

    spec version update

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -696,7 +696,7 @@ class Configuration
         $report  = 'PHP SDK (XeroAPI\XeroPHP) Debug Report:' . PHP_EOL;
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
-        $report .= '    OpenAPI Spec Version: 8.0.0' . PHP_EOL;
+        $report .= '    OpenAPI Spec Version: 8.1.0' . PHP_EOL;
         $report .= '    SDK Package Version: 9.1.0' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
