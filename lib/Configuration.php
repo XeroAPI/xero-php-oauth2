088ede5133200e970bb9e9e92fde52d077f807b1
sangeet-joy-tw <157606431+sangeet-joy-tw@users.noreply.github.com>
2025-06-28 18:23:29 +0530

    PHP SDK version update 10.0.0 (#397)
    Ruby SDK | version update 12.0.0 (#344)

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -696,8 +696,8 @@ class Configuration
         $report  = 'PHP SDK (XeroAPI\XeroPHP) Debug Report:' . PHP_EOL;
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
-        $report .= '    OpenAPI Spec Version: 8.3.0' . PHP_EOL;
-        $report .= '    SDK Package Version: 9.3.0' . PHP_EOL;
+        $report .= '    OpenAPI Spec Version: 9.0.0' . PHP_EOL;
+        $report .= '    SDK Package Version: 10.0.0' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
